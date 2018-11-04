00c35c346c77ed4e72d0533a533c05ae6c84c0d0
Ivan Shvedunov <ivan4th@gmail.com>
2018-11-04 17:48:37 +0300

    Add more or less workable TBH solution

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,54 @@
 #include <Controllino.h>
 #include <SPI.h>
 #include <Ethernet.h>
+#include <PubSubClient.h>
+
+#include "tbh.h"
+
+const int minX = 65;
+const int maxX = 344;
+const int maxPWM = 255;
+const float maxTargetTemp = 100;
+const float minTargetTemp = 0;
+const float noControlledTemp = -1000;
+
+// good/possible good for tank->house outflow stabilization:
+// static constexpr double defaultTargetTemp = 25;
+// static constexpr double defaultKp = 0.07;// good: 0.001; then: 0.02; 0.05 better; 0.08 even better
+// static constexpr double defaultKi = 0.0004;// ~good: 0.0003;
+
+static constexpr double defaultTargetTemp = 24;
+static constexpr double defaultKp = 0.07;
+static constexpr double defaultKi = 0.0001; // acceptable: 6
+
+static int defaultTbhInterval = 1000;
+
+TBH tbh(defaultTargetTemp, defaultKp, defaultKi, defaultTbhInterval);
+
+float targetTemp = defaultTargetTemp;
+bool controlTemp = true;
 
 const int maxNTemps = 16;
-float temps[maxNTemps];
-DeviceAddress addrs[maxNTemps];
+struct tempItem {
+    DeviceAddress addr;
+    float t;
+    const char* name;
+};
+tempItem tempItems[maxNTemps];
 int curNTemps = 0, curX = 0, curY = 0;
 
 byte mac[] = {
-              0xDE, 0xAD, 0xBE, 0x12, 0x33, 0x55
+    0xDE, 0xAD, 0xBE, 0x12, 0x33, 0x55
 };
-IPAddress ip(192, 168, 20, 80);
+// IPAddress ip(192, 168, 20, 80);
+IPAddress brokerIP(192, 168, 20, 61);
 
-EthernetServer server(80);
+// Callback function header
+void callback(char* topic, byte* payload, unsigned int length);
+
+EthernetClient ethClient;
+// EthernetServer server(80);
+PubSubClient client(brokerIP, 1883, callback, ethClient);
 
 /********************************************************************/
 #define PWM_PIN CONTROLLINO_D0
@@ -33,26 +69,128 @@ DallasTemperature sensors(&oneWire);
 /********************************************************************/
 
 struct addrDesc {
-    char* addr;
-    char* desc;
+    DeviceAddress addr;
+    const char* desc;
+    bool controlled;
 };
 
 struct addrDesc addrDescs[] = {
-                               {"280e2a7107000015", "board temp"},
-                               {"28ffffffb6ffd622", "boiler->tank"},
-                               {"28ffff62fff52217", "tank->boiler"},
-                               {"28ffff0effdb2217", "tank->house"},
-                               {"28ffffff80082317", "house->tank"},
-                               {"", ""}
+    { { 0x28, 0x0e, 0x2a, 0x71, 0x07, 0x00, 0x00, 0x15 }, "temp-board", false},
+    { { 0x28, 0xff, 0xb6, 0xd6, 0x22, 0x17, 0x04, 0xab }, "temp-boiler-to-tank", false},
+    { { 0x28, 0xff, 0x62, 0xf5, 0x22, 0x17, 0x04, 0x12 }, "temp-tank-to-boiler", false},
+    { { 0x28, 0xff, 0x0e, 0xdb, 0x22, 0x17, 0x04, 0x47 }, "temp-tank-to-house", false}, // was: true
+    { { 0x28, 0xff, 0x80, 0x08, 0x23, 0x17, 0x04, 0x0c }, "temp-house-to-tank", true}, // was: false
+    { { 0x28, 0xff, 0xc0, 0x62, 0xa6, 0x16, 0x03, 0x7e }, "temp-tank-a", false},
+    { { 0x28, 0xff, 0x6b, 0xa0, 0x24, 0x17, 0x03, 0xea }, "temp-tank-b", false},
+    { { 0x28, 0xff, 0xaf, 0x3e, 0x81, 0x14, 0x02, 0xeb }, "temp-tank-c", false},
+    { {}, 0}
 };
 
-char* getSensorName(char* addr) {
-    for (int n = 0; *addrDescs[n].addr; n++) {
+const char* getSensorName(uint8_t* addr, bool* controlled) {
+    for (int n = 0; addrDescs[n].desc; n++) {
         addrDesc* d = addrDescs + n;
-        if (!strcmp(addr, d->addr))
-            return d->desc;
+        if (memcmp(addr, d->addr, 8))
+            continue;
+        if (controlled)
+            *controlled = d->controlled;
+        return d->desc;
     }
     return 0;
+}
+
+
+void publishFloat(const char* name, double v) {
+    char topic[128];
+    snprintf(topic, 128, "/devices/boiler/controls/%s", name);
+    char valueStr[64];
+    dtostrf(v, 1, 2, valueStr);
+    client.publish(topic, valueStr);
+}
+
+void publishCur() {
+    publishFloat("valveY", curY * 100.0 / maxPWM);
+    publishFloat("targetTemp", targetTemp);
+    publishFloat("ki", tbh.ki() * 1000);
+    publishFloat("kp", tbh.kp() * 1000);
+    publishFloat("interval", tbh.interval());
+}
+
+void setTargetTemp(double v) {
+    controlTemp = true;
+    targetTemp = v;
+    if (targetTemp > maxTargetTemp)
+        targetTemp = maxTargetTemp;
+    else if (targetTemp < minTargetTemp)
+        targetTemp = minTargetTemp;
+    tbh.setTarget(targetTemp);
+}
+
+void setCurY(double v) {
+    controlTemp = false;
+    curY = v * maxPWM / 100.;
+    if (curY < 0)
+        curY = 0;
+    else if (curY > maxPWM)
+        curY = maxPWM;
+}
+
+void setKi(double v) {
+    controlTemp = true;
+    tbh.setKi(v / 1000);
+}
+
+void setKp(double v) {
+    controlTemp = true;
+    tbh.setKp(v / 1000);
+}
+
+void setInterval(double v) {
+    controlTemp = true;
+    tbh.setInterval((unsigned long)v);
+}
+
+typedef void (*SetValueFunc)(double);
+
+struct TopicItem {
+    const char* topic;
+    SetValueFunc handler;
+};
+
+TopicItem topicItems[] = {
+    { "/devices/boiler/controls/targetTemp/on", setTargetTemp },
+    { "/devices/boiler/controls/valveY/on", setCurY },
+    { "/devices/boiler/controls/ki/on", setKi },
+    { "/devices/boiler/controls/kp/on", setKp },
+    { "/devices/boiler/controls/interval/on", setInterval },
+    { 0, 0 },
+};
+
+TopicItem* getTopicItem(const char *topic) {
+    for (TopicItem* item = topicItems; item->topic; item++) {
+        if (!strcmp(topic, item->topic))
+            return item;
+    }
+    return 0;
+}
+
+// Callback function
+void callback(char* topic, byte* payload, unsigned int length) {
+    TopicItem* topicItem = getTopicItem(topic);
+    if (!topicItem) {
+        Serial.print("WARNING: unhandled topic: ");
+        Serial.println(topic);
+        return;
+    }
+    char* buf = (char*)malloc(length + 1);
+    buf[length] = 0;
+    memcpy(buf, payload, length);
+    Serial.print("*** Accepting value: ");
+    Serial.print(topic);
+    Serial.print("=");
+    Serial.println(buf);
+    topicItem->handler(atof(buf));
+    publishCur();
+    free(buf);
 }
 
 void setup(void) 
@@ -63,8 +201,8 @@ void setup(void)
     // digitalWrite(CONTROLLINO_RTC_CHIP_SELECT, LOW);       // inactive
     // digitalWrite(CONTROLLINO_ETHERNET_CHIP_SELECT, HIGH); // inactive
  
-    for (int i = 0; i < maxNTemps; i++)
-        temps[i] = 0;
+    // for (int i = 0; i < maxNTemps; i++)
+    //     temps[i].t = 0;
     // start serial port 
     Serial.begin(115200); 
 
@@ -76,6 +214,10 @@ void setup(void)
     //Ethernet.begin(mac, ip);
     if (!Ethernet.begin(mac))
         Serial.println("ethernet fail...");
+    else {
+        Serial.print("local ip: ");
+        Serial.println(Ethernet.localIP());
+    }
 
     // Check for Ethernet hardware present
     //if (Ethernet.hardwareStatus() == EthernetNoHardware) {
@@ -89,138 +231,40 @@ void setup(void)
     //}
 
     // start the server
-    server.begin();
-    Serial.print("server is at ");
-    Serial.println(Ethernet.localIP());
+    // server.begin();
+    // Serial.print("server is at ");
+    // Serial.println(Ethernet.localIP());
+
+    
+    Ethernet.begin(mac);
+    if (client.connect("arduinoClient")) {
+        client.publish("/devices/boiler/msg", "booted");
+        // client.subscribe("/devices/boiler/controls/valveY/on");
+        for (TopicItem* item = topicItems; item->topic; item++)
+            client.subscribe(item->topic);
+    }
 }
 
 enum reqState {
-               ReqStart,
-               ReqQuery,
-               ReqNewLine,
-               ReqNonblankLine,
-               ReqDone
+    ReqStart,
+    ReqQuery,
+    ReqNewLine,
+    ReqNonblankLine,
+    ReqDone
 };
-
-void writeResponse(EthernetClient* client) {
-    // send a standard http response header
-    client->println("HTTP/1.1 200 OK");
-    client->println("Content-Type: text/html");
-    client->println("Connection: close");  // the connection will be closed after completion of the response
-    client->println("Refresh: 15");  // refresh the page automatically every 5 sec
-    client->println();
-    client->println("<!DOCTYPE HTML>");
-    client->println("<html>");
-    // output the value of each analog input pin
-    for (int n = 0; n < curNTemps; n++) {
-        //int sensorReading = analogRead(analogChannel);
-        char addrStr[17];
-        char* addr = addrs[n];
-        snprintf(addrStr, 17, "%02x%02x%02x%02x%02x%02x%02x%02x",
-                 addr[0],
-                 addr[1],
-                 addr[2],
-                 addr[3],
-                 addr[4],
-                 addr[5],
-                 addr[6],
-                 addr[7]);
-        char* sensorName = getSensorName(addrStr);
-        if (!sensorName)
-            sensorName = "temp";
-        client->print(sensorName);
-        client->print("=");
-        client->print(temps[n]);
-        client->print("; address=");
-        client->print(addrStr);
-
-        client->println("<br />");
-    }
-    client->print("x = ");
-    client->print(curX);
-    client->print("; ");
-    client->print("y = ");
-    client->print(curY);
-    client->println();
-    client->println("</html>");
-}
-
-void handleEthernet() {
-    // listen for incoming clients
-    EthernetClient client = server.available();
-    if (client) {
-        Serial.println("new client");
-        // an http request ends with a blank line
-        //bool currentLineIsBlank = true;
-        reqState s = ReqStart;
-        char digits[16];
-        int nDigits = 0;
-        while (client.connected() && s != ReqDone) {
-            if (client.available()) {
-                char c = client.read();
-                Serial.write(c);
-                switch (s) {
-                case ReqStart:
-                    //Serial.print("reqStart: ");
-                    //Serial.println(c);
-                    if (c == '\n')
-                        s = ReqNewLine;
-                    else if (c == '?')
-                        s = ReqQuery;
-                    break;
-                case ReqQuery:
-                    //Serial.print("reqQuery: ");
-                    //Serial.println(c);
-                    if (c == '\n') {
-                        s = ReqNewLine;
-                    } else if (isdigit(c)) {
-                        digits[nDigits++] = c;
-                        if (nDigits == 15)
-                            s = ReqNonblankLine;
-                    } else
-                        s = ReqNonblankLine;
-                    break;
-                case ReqNewLine:
-                    //Serial.print("reqNewLine: ");
-                    //Serial.println(c);
-                    if (c == '\n') {
-                        //Serial.print("nDigits=");
-                        //Serial.println(nDigits);
-                        if (nDigits > 0) {
-                            digits[nDigits] = 0;
-                            curY = atoi(digits);
-                            if (curY < 0)
-                                curY = 0;
-                            else if (curY > 255)
-                                curY = 255;
-                        }
-                        writeResponse(&client);
-                        s = ReqDone;
-                    } else if (c != '\r')
-                        s = ReqNonblankLine;
-                    break;
-                case ReqNonblankLine:
-                    //Serial.print("reqNonblankLine: ");
-                    //Serial.println(c);
-                    if (c == '\n')
-                        s = ReqNewLine;
-                    break;
-                }
-            }
-        }
-        // give the web browser time to receive the data
-        delay(1);
-        // close the connection:
-        client.stop();
-        Serial.println("client disconnected");
-    }
-}
 
 void loop(void) 
 {
-    handleEthernet();
+    client.loop();
+
+    // handleEthernet();
     ///Serial.print("x: ");
     curX = analogRead(X_PIN);
+    float x = (curX - minX) * 100.0 / (maxX - minX);
+    x = max(0, min(x, 100));
+    publishFloat("valveX", x);
+    publishCur();
+
     ///Serial.println(curX);
     // call sensors.requestTemperatures() to issue a global temperature 
     // request to all devices on the bus 
@@ -234,29 +278,64 @@ void loop(void)
         count = maxNTemps;
     curNTemps = count;
     float maxT = 0;
+    float controlledTemp = noControlledTemp;
     for (int i = 0; i < count; i++) {
         ///Serial.print(i);
         ///Serial.print(": t = "); 
-        float t = sensors.getTempCByIndex(i);
+        sensors.getAddress(tempItems[i].addr, i);
+        DeviceAddress& addr = tempItems[i].addr;
+        float t = sensors.getTempC(tempItems[i].addr);
+        // if (t == DEVICE_DISCONNECTED_C) {
+        //     // ...
+        //     continue;
+        // }
         if (t > maxT)
             maxT = t;
         ///Serial.println(t);
-        temps[i] = t;
-        sensors.getAddress(addrs[i], i);
+        tempItems[i].t = t;
+
+        bool controlled;
+        tempItems[i].name = getSensorName(tempItems[i].addr, &controlled);
+        if (!tempItems[i].name) {
+            char addrStr[64];
+            DeviceAddress& addr = tempItems[i].addr;
+            snprintf(addrStr, 64, "Unrecognized address: %02x%02x%02x%02x%02x%02x%02x%02x",
+                     addr[0], addr[1], addr[2], addr[3],
+                     addr[4], addr[5], addr[6], addr[7]);
+            Serial.println(addrStr);
+        } else if (controlled)
+            controlledTemp = t;
+
+        char addrStr[64];
+        snprintf(addrStr, 64, "Got addr: %02x%02x%02x%02x%02x%02x%02x%02x; name = %s; temp = ",
+                 addr[0], addr[1], addr[2], addr[3],
+                 addr[4], addr[5], addr[6], addr[7],
+                 tempItems[i].name ? tempItems[i].name : "<none>");
+        Serial.print(addrStr);
+        Serial.println(t);
+
     }
-    /*
-      float coef = (maxT - 23) / 10.0;
-      int target = (1 - coef) * 255;
-      if (target < 0)
-      target = 0;
-      else if (target > 255)
-      target = 255;
-      curY = target;
-    */
-    ///target=0;
-    analogWrite(PWM_PIN, curY);
-    ///Serial.print("target: ");
-    ///Serial.println(curY);
-    ///Serial.println("");
-    //delay(200); 
+
+    if (controlledTemp == noControlledTemp)
+        Serial.println("WARNING: couldn't read the controlled temp");
+    else {
+        if (controlTemp && tbh.handle(controlledTemp, millis())) {
+            curY = tbh.output() * maxPWM;
+            if (curY < 0)
+                curY = 0;
+            else if (curY > maxPWM)
+                curY = maxPWM;
+        }
+        analogWrite(PWM_PIN, curY);
+        publishCur();
+    }
+
+    for (int i = 0; i < count; i++) {
+        if (!tempItems[i].name || tempItems[i].t == DEVICE_DISCONNECTED_C)
+            continue;
+        publishFloat(tempItems[i].name, tempItems[i].t);
+    }
 } 
+
+// TODO: fix reading sensor data
+// TODO: reconnect
---
+++ src/tbh.cpp
@@ -0,0 +1,68 @@
+#include <Arduino.h>
+
+#include "tbh.h"
+
+TBH::TBH(double setPoint, double kp, double ki, unsigned long interval):
+    _setPoint(setPoint), _kp(kp), _ki(ki), _interval(interval) {}
+
+bool TBH::handle(double input, unsigned long t)
+{
+    if (t < _nextUpdate)
+        return false;
+    double err = _setPoint - input;
+    double fScaled = _ki * double(t - _nextUpdate + _interval) / _interval;
+    _nextUpdate = t + _interval;
+    _intTerm += fScaled * err;
+    Serial.print("TBH: preliminary int term = ");
+    Serial.println(_intTerm);
+    if (_gotLastInput &&
+        ((_lastInput < _setPoint && input >= _setPoint) ||
+         (_lastInput > _setPoint && input <= _setPoint))) {
+        if (_gotZeroCross) {
+            Serial.print("TBH: zero cross detected, last output =");
+            Serial.println(_lastZeroCrossIntTerm);
+            _intTerm = (_intTerm + _lastZeroCrossIntTerm) / 2;
+        } else {
+            Serial.println("TBH: first zero cross detected");
+            _gotZeroCross = true;
+        }
+        _lastZeroCrossIntTerm = _intTerm;
+    }
+    _lastInput = input;
+    _gotLastInput = true;
+    if (_intTerm < minIntTerm)
+        _intTerm = 0;
+    else if (_intTerm > maxIntTerm)
+        _intTerm = 1;
+    _output = _kp * err + _intTerm;
+    if (_output < 0)
+        _output = 0;
+    else if (_output > 1)
+        _output = 1;
+    Serial.print("TBH: ki*1000 = ");
+    Serial.print(_ki * 1000);
+    Serial.print(", kp*1000 = ");
+    Serial.print(_kp * 1000);
+    Serial.print(", fScaled = ");
+    Serial.print(fScaled);
+    Serial.print(", t = ");
+    Serial.print(t);
+    Serial.print(", interval = ");
+    Serial.println(_interval);
+
+    Serial.print("TBH: input = ");
+    Serial.print(input);
+    Serial.print(", setPoint = ");
+    Serial.print(_setPoint);
+    Serial.print(", err = ");
+    Serial.print(err);
+    Serial.print(", intTerm = ");
+    Serial.print(_intTerm);
+    Serial.print(", output = ");
+    Serial.println(_output);
+
+    // log("TBH: ki = %f, kp = %f, fScaled = %f, t = %ld, interval = %ld", _ki, _kp, fScaled, t, _interval);
+    // log("TBH: input = %f, setPoint = %f, err = %f, intTerm = %f, output = %f", input, _setPoint, err, _intTerm, _output);
+
+    return true;
+}
